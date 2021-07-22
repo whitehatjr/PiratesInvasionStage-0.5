d46f08b56b4a6a6f57164098e53d0653929d70ba
whitehatjr <rajeev@whitehatjr.com>
2021-07-22 20:07:28 +0530

    updated code

--- sketch.js
+++ sketch.js
@@ -5,8 +5,9 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var canvas, angle, tower, ground, cannon;
 
+
+
 function preload() {
-  backgroundImg = loadImage("./assets/background.gif");
   towerImage = loadImage("./assets/tower.png");
 }
 
@@ -14,14 +15,26 @@ function setup() {
   canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-  tower = new Tower(150, 350, 160, 310);
+
+  var options={
+    isStatic:true
+  }
+
+  ground = Bodies.rectangle(0,590,0,0,options)
+  World.add(world,ground)
+
+  tower = Bodies.rectangle(80,270,0,0,options)
+
 }
+
+
 
 function draw() {
   background(189);
-  image(backgroundImg, 0, 0, width, height);
 
   Engine.update(engine);
+  rect(ground.position.x,ground.position.y,1200,1);
 
-  tower.display();
+  image(towerImage,tower.position.x,tower.position.y,160,320)
+
 }
